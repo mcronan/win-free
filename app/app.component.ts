cb0c3548eb44184dadf39dfc964a09365be578ba
Ronan <ronanir600@gmail.com>
2016-03-26 18:02:33 +0000

    new push

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,36 @@
-import {Component} from 'angular2/core';
+import {Component, bind} from 'angular2/core';
+import {LandingComponent} from './landing.component';
+import {VideoComponent} from './video.component';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+
+
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+    	<h1>Hello</h1>
+	    <h1>{{title}}</h1>
+  		<router-outlet></router-outlet>
+  		`,
+    directives: [ROUTER_DIRECTIVES],
+  	providers: [ROUTER_PROVIDERS]
 })
-export class AppComponent { }
+	    
+
+@RouteConfig({
+	{
+		path: '/landing',
+		name: 'Landing',
+		component: LandingComponent,
+		useAsDefault: true
+	},
+	{
+		path: '/video',
+		name: 'Video', 
+		component: VideoComponent
+	}
+})
+
+export class AppComponent { 
+	 title = 'Win Free!';
+}
---
+++ app/landing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from 'angular2/core';
+import { Router } from 'angular2/router';
+
+@Component({ 
+	selector: 'my-landing',
+	templateUrl: 'app/landing.component.html',
+	styleUrls: ['app/landing.component.css']
+})
+
+export class LandingComponent {}
