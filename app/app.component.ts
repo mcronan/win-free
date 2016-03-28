c6cb4362a191138188aa6f1b46eb475dfa797063
Ronan <ronanir600@gmail.com>
2016-03-28 14:56:59 +0100

    router working!

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,35 +1,36 @@
-import {Component, bind} from 'angular2/core';
-import {LandingComponent} from './landing.component';
-import {VideoComponent} from './video.component';
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
-
-
+import { Component } from 'angular2/core';
+import { LandingComponent } from './landing.component';
+import { VideoComponent } from './video.component';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 
 @Component({
     selector: 'my-app',
     template: `
     	<h1>Hello</h1>
 	    <h1>{{title}}</h1>
+	    <nav>
+	   	 <a [routerLink]="['Landing']">Landing</a>
+	   	 <a [routerLink]="['Video']">Video</a>
+	    </nav>
   		<router-outlet></router-outlet>
   		`,
-    directives: [ROUTER_DIRECTIVES],
-  	providers: [ROUTER_PROVIDERS]
+    directives: [ROUTER_DIRECTIVES]
 })
-	    
 
-@RouteConfig({
+@RouteConfig([
 	{
 		path: '/landing',
 		name: 'Landing',
 		component: LandingComponent,
-		useAsDefault: true
+		// useAsDefault: true
 	},
 	{
 		path: '/video',
-		name: 'Video', 
-		component: VideoComponent
+		name: 'Video',
+		component: VideoComponent,
+		// useAsDefault: true
 	}
-})
+])
 
 export class AppComponent { 
 	 title = 'Win Free!';
--- app/video.component.ts
+++ app/video.component.ts
@@ -1,10 +1,12 @@
 import { Component } from 'angular2/core';
-import { Router } from 'angular2/router';
+// import { Router } from 'angular2/router';
 
 
 @Component({
 	selector: 'my-video',
-	templateUrl: 'app/video.component.html'
+	templateUrl: 'app/video.component.html',
+	styleUrls: ['app/landing.component.css']
+
 })
 
 export class VideoComponent {}
